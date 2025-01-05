d09a52d9f06cb679660db1fcd13e9f16d11e3f6c
HombreOso <arthur.erich.emig@gmail.com>
2025-01-05 02:01:42 +0100

    allow headers

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(express.json()); // To parse JSON bodies
 app.use(CORS({
   origin: 'https://test-wishlist-tentech.myshopify.com', // Exact Shopify store URL
   methods: ['GET', 'POST', 'OPTIONS'], // Include OPTIONS for preflight
-  allowedHeaders: ['Content-Type', 'Authorization'], // Specify allowed headers
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Shopify-Access-Token', 'Access-Control-Allow-Origin', 'Access-Control-Allow-Methods'], // Specify allowed headers
   credentials: true, // Allow cookies and other credentials
 }));
 // Optionally, enable CORS for your frontend domain
